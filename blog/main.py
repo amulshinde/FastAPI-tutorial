e0b5a6568d26f949cbb32db559985239b8e8a930
amulshinde <amulscindia@gmail.com>
2025-04-02 20:20:50 +0530

    hashing password

--- blog/main.py
+++ blog/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
+
 
 app = FastAPI()
 
@@ -63,9 +65,12 @@ def update(id, request : schemas.Blog, db: Session = Depends(get_db) ):
     return {"message": "title updated successfully"}
 
 
+
+
+
 @app.post('/user')
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name= request.name, email = request.email, password = request.password)
+    new_user = models.User(name= request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
