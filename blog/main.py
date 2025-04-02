2a7f524e5f33b2978e7eb570641a91003304d567
amulshinde <amulscindia@gmail.com>
2025-04-02 21:19:58 +0530

    show-user info, hide password

--- blog/main.py
+++ blog/main.py
@@ -67,8 +67,7 @@ def update(id, request : schemas.Blog, db: Session = Depends(get_db) ):
 
 
 
-
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name= request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
